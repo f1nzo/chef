a30b42f532c9045f9c1b3fd686b6aef2321f9e40
fin <finwitherspoon@duck.com>
2024-03-12 22:03:48 -0700

    added cute bouncy arrow on home screen

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,8 +1,18 @@
+
+
 <div class="hero w-screen h-screen bg-base-200 overflow-hidden">
     <div class="hero-content text-center">
         <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Welcome Chef! :)</h1>
-        <p class="py-6">Try adding add some recipes to get started meal planning. </p>
+            <h1 class="text-5xl font-bold">Welcome Chef! :)</h1>
+            <p class="py-6">Try adding add some recipes to get started meal planning.</p>
         </div>
     </div>
-</div>    
+    <!-- Create a flex container to center the SVG horizontally -->
+    <div class="flex justify-center fixed bottom-16 w-full pb-2">
+        <!-- Remove flex and justify-center classes from the SVG element -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 animate-bounce">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+</svg>
+
+    </div>
+</div>
